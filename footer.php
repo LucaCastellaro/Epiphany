9b2c245f5592ef8bcee83a24cb65d6b7153edb47
MatteoBosi <matteo.bosi95@hotmail.it>
2015-04-15 20:36:51 +0200

    v150415.2035
    ho tolto il footer e l'ho messo in una pagina a parte cosi va modificato
    in un solo posto.
    ho creato la pagina per la registrazione e il login, ma non l'ho
    completata

---
+++ footer.php
@@ -0,0 +1,133 @@
+<div id="footer-wrapper">
+				<section id="footer" class="container">
+					<div class="row">
+						<div class="8u">
+							<section>
+								<header>
+									<h2>Blandit nisl adipiscing</h2>
+								</header>
+								<ul class="dates">
+									<li>
+										<span class="date">Jan <strong>27</strong></span>
+										<h3><a href="#">Lorem dolor sit amet veroeros</a></h3>
+										<p>Ipsum dolor sit amet veroeros consequat blandit ipsum phasellus lorem consequat etiam.</p>
+									</li>
+									<li>
+										<span class="date">Jan <strong>23</strong></span>
+										<h3><a href="#">Ipsum sed blandit nisl consequat</a></h3>
+										<p>Blandit phasellus lorem ipsum dolor tempor sapien tortor hendrerit adipiscing feugiat lorem.</p>
+									</li>
+									<li>
+										<span class="date">Jan <strong>15</strong></span>
+										<h3><a href="#">Magna tempus lorem feugiat</a></h3>
+										<p>Dolore consequat sed phasellus lorem sed etiam nullam dolor etiam sed amet sit consequat.</p>
+									</li>
+									<li>
+										<span class="date">Jan <strong>12</strong></span>
+										<h3><a href="#">Dolore tempus ipsum feugiat nulla</a></h3>
+										<p>Feugiat lorem dolor sed nullam tempus lorem ipsum dolor sit amet nullam consequat.</p>
+									</li>
+									<li>
+										<span class="date">Jan <strong>10</strong></span>
+										<h3><a href="#">Blandit tempus aliquam?</a></h3>
+										<p>Feugiat sed tempus blandit tempus adipiscing nisl lorem ipsum dolor sit amet dolore.</p>
+									</li>
+								</ul>
+							</section>
+						</div>
+						<div class="4u">
+							<section>
+								<header>
+									<h2>Informazioni</h2>
+								</header>
+								<a href="#" class="image featured"><img src="images/pic10.jpg" alt="" /></a>
+								<p>
+									<p>Questo &egrave; <strong>Epiphany</strong>.</p>
+									<p>Hai una nuova idea innovativa ma non sai come realizzarla?<br>Inseriscila nel nostro database e aspetta che qualcun altro ti aiuti!</p>
+									<p>Hai delle grandi abilit&agrave; ma al momento non sai come usarle?<br>Cerca un'idea e aiuta qualcuno a risolvere un problema!</p>
+								</p>
+								<footer>
+									<a href="" class="button">Leggi altro su Epiphany</a>
+								</footer>
+							</section>
+						</div>
+					</div>
+					<div class="row">
+						<div class="4u">
+							<section>
+								<header>
+									<h2>Tempus consequat</h2>
+								</header>
+								<ul class="divided">
+									<li><a href="#">Lorem ipsum dolor sit amet sit veroeros</a></li>
+									<li><a href="#">Sed et blandit consequat sed tlorem blandit</a></li>
+									<li><a href="#">Adipiscing feugiat phasellus sed tempus</a></li>
+									<li><a href="#">Hendrerit tortor vitae mattis tempor sapien</a></li>
+									<li><a href="#">Sem feugiat sapien id suscipit magna felis nec</a></li>
+									<li><a href="#">Elit class aptent taciti sociosqu ad litora</a></li>
+								</ul>
+							</section>
+						</div>
+						<div class="4u">
+							<section>
+								<header>
+									<h2>Ipsum et phasellus</h2>
+								</header>
+								<ul class="divided">
+									<li><a href="#">Lorem ipsum dolor sit amet sit veroeros</a></li>
+									<li><a href="#">Sed et blandit consequat sed tlorem blandit</a></li>
+									<li><a href="#">Adipiscing feugiat phasellus sed tempus</a></li>
+									<li><a href="#">Hendrerit tortor vitae mattis tempor sapien</a></li>
+									<li><a href="#">Sem feugiat sapien id suscipit magna felis nec</a></li>
+									<li><a href="#">Elit class aptent taciti sociosqu ad litora</a></li>
+								</ul>
+							</section>
+						</div>
+						<div class="4u">
+							<section>
+								<header>
+									<h2>Vitae tempor lorem</h2>
+								</header>
+								<ul class="social">
+									<li><a class="icon fa-facebook" href="#"><span class="label">Facebook</span></a></li>
+									<li><a class="icon fa-twitter" href="#"><span class="label">Twitter</span></a></li>
+									<li><a class="icon fa-dribbble" href="#"><span class="label">Dribbble</span></a></li>
+									<li><a class="icon fa-linkedin" href="#"><span class="label">LinkedIn</span></a></li>
+									<li><a class="icon fa-google-plus" href="#"><span class="label">Google+</span></a></li>
+								</ul>
+								<ul class="contact">
+									<li>
+										<h3>Mail</h3>
+										<p>
+                                                                                    <input type="text" name="contact-mail" id="contact-mail" placeholder="Mail">									
+										</p>
+									</li>
+									<li>
+										<h3>Nome</h3>
+										<p><input type="text" name="contact-name" id="contact-name" placeholder="Nome"></p>
+									</li>
+									<li>
+										<h3>Messaggio</h3>
+                                                                                <p><textarea name="contact-msg" id="contact-msg" rows="6"></textarea></p>
+									</li>
+                                                                        <li>
+                                                                            <p><input type="button" name="bt_contact" id="bt_contact" value="Invia"></p>
+									</li>
+								</ul>
+							</section>
+						</div>
+					</div>
+					<div class="row">
+						<div class="12u">
+						
+							<!-- Copyright -->
+								<div id="copyright">
+									<ul class="links">
+										<li>&copy; 2015 - Epiphany. All rights reserved.</li><li>Powered by <a href="lucacastellaro.altervista.org">Luca Castellaro</a>, <a href="#">Matteo Bosi</a></li>
+									</ul>
+								</div>
+
+						</div>
+					</div>
+				</section>
+			</div>
---
+++ reg-loginform.php
@@ -0,0 +1,175 @@
+<!DOCTYPE HTML>
+<!--
+	Dopetrope by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Epiphany</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <link href='http://fonts.googleapis.com/css?family=Sacramento' rel='stylesheet' type='text/css'> <!--Font per il logo Epiphany-->
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+
+		<!-- Header -->
+			<div id="header-wrapper">
+				<div id="header">
+					
+					<!-- Logo -->
+						<h1><a href="">Epiphany</a></h1>
+					
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li><a href="index.php">Home</a></li>
+<!--								<li>
+									<a href="">Categorie</a>
+									<ul>
+										<li><a href="#">Lorem ipsum dolor</a></li>
+										<li><a href="#">Magna phasellus</a></li>
+										<li><a href="#">Etiam dolore nisl</a></li>
+										<li>
+											<a href="">Phasellus consequat</a>
+											<ul>
+												<li><a href="#">Magna phasellus</a></li>
+												<li><a href="#">Etiam dolore nisl</a></li>
+												<li><a href="#">Veroeros feugiat</a></li>
+												<li><a href="#">Nisl sed aliquam</a></li>
+												<li><a href="#">Dolore adipiscing</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Veroeros feugiat</a></li>
+									</ul>
+								</li>
+-->								<li><a href="left-sidebar.html">Esplora</a></li>
+<li id="li-cerca"><a href=""><input type="text" name="cerca" id="cerca" placeholder="Cerca un'idea"> <input type="button" name="bt_cerca" id="bt_cerca" value="Cerca"></a></li>
+<!--                                                      		<li><a href="right-sidebar.html">Crea</a></li>				
+							
+                            				COMMENTO IMPORTANTE: il pulsante "crea" pulsante facciamolo "vedere" solo se è già stato fatto il login (:
+                                            
+-->                                                      	<li class="current"><a href="">Registrazione / Login</a></li>
+                                                               
+							</ul>
+						</nav>				
+				</div>
+			</div>
+		
+		<!-- Main -->
+			<div id="main-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="8u">
+							
+							<!-- Content -->
+								<article class="box post">
+									<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+									<header>
+										<h2>Registrati</h2>
+										<p>Compila il form per poterti registrare al nostro portale</p>
+									</header>
+									<p>
+										Vestibulum scelerisque ultricies libero id hendrerit. Vivamus malesuada quam faucibus ante dignissim auctor 
+										hendrerit libero placerat. Nulla facilisi. Proin aliquam felis non arcu molestie at accumsan turpis commodo. 
+										Proin elementum, nibh non egestas sodales, augue quam aliquet est, id egestas diam justo adipiscing ante. 
+										Pellentesque tempus nulla non urna eleifend ut ultrices nisi faucibus.
+									</p>
+									<p>
+										Praesent a dolor leo. Duis in felis in tortor lobortis volutpat et pretium tellus. Vestibulum ac ante nisl, 
+										a elementum odio. Duis semper risus et lectus commodo fringilla. Maecenas sagittis convallis justo vel mattis. 
+										placerat, nunc diam iaculis massa, et aliquet nibh leo non nisl vitae porta lobortis, enim neque fringilla nunc, 
+										eget faucibus lacus sem quis nunc suspendisse nec lectus sit amet augue rutrum vulputate ut ut mi. Aenean 
+										elementum, mi sit amet porttitor lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor
+										sit amet nullam consequat feugiat dolore tempus.
+									</p>
+									<section>
+										<header>
+											<h3>Something else</h3>
+										</header>
+										<p>
+											Elementum odio duis semper risus et lectus commodo fringilla. Maecenas sagittis convallis justo vel mattis. 
+											placerat, nunc diam iaculis massa, et aliquet nibh leo non nisl vitae porta lobortis, enim neque fringilla nunc, 
+											eget faucibus lacus sem quis nunc suspendisse nec lectus sit amet augue rutrum vulputate ut ut mi. Aenean 
+											elementum, mi sit amet porttitor lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor
+											sit amet nullam consequat feugiat dolore tempus.
+										</p>
+										<p>
+											Nunc diam iaculis massa, et aliquet nibh leo non nisl vitae porta lobortis, enim neque fringilla nunc, 
+											eget faucibus lacus sem quis nunc suspendisse nec lectus sit amet augue rutrum vulputate ut ut mi. Aenean 
+											elementum, mi sit amet porttitor lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor
+											sit amet nullam consequat feugiat dolore tempus.
+										</p>
+									</section>
+									<section>
+										<header>
+											<h3>So in conclusion ...</h3>
+										</header>
+										<p>
+											Nunc diam iaculis massa, et aliquet nibh leo non nisl vitae porta lobortis, enim neque fringilla nunc, 
+											eget faucibus lacus sem quis nunc suspendisse nec lectus sit amet augue rutrum vulputate ut ut mi. Aenean 
+											elementum, mi sit amet porttitor lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor
+											sit amet nullam consequat feugiat dolore tempus. Elementum odio duis semper risus et lectus commodo fringilla. 
+											Maecenas sagittis convallis justo vel mattis. 
+										</p>
+									</section>
+								</article>
+
+						</div>
+						<div class="4u">
+						
+							<!-- Sidebar -->
+								<section class="box">
+									<a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
+									<header>
+										<h3>Login</h3>
+									</header>
+									<p>
+                                    	<form name="form-login" id="form_login" method="post" action="">
+	                                    	<input type="text" name="user_login" id="user_login" placeholder="Nome Utente"><br>
+	                                    	<input type="password" name="password_login" id="password_login" placeholder="Password">
+                                    </p>
+									<footer>
+	                                        <input type="button" name="conferma_login" id="conferma_login" value="Login">
+                                        </form>
+
+									</footer>
+								</section>
+								<!--<section class="box">
+									<header>
+										<h3>Feugiat consequat</h3>
+									</header>
+									<p>Veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat sed lorem consequat feugiat lorem dolore.</p>
+									<ul class="divided">
+										<li><a href="#">Sed et blandit consequat sed</a></li>
+										<li><a href="#">Hendrerit tortor vitae sapien dolore</a></li>
+										<li><a href="#">Sapien id suscipit magna sed felis</a></li>
+										<li><a href="#">Aptent taciti sociosqu ad litora</a></li>
+									</ul>
+									<footer>
+										<a href="#" class="button alt">Ipsum consequat</a>
+									</footer>
+								</section>-->
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+		<!-- Footer -->
+			<?php include("footer.php");?>
+	</body>
+</html>
